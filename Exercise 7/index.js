2be378807870c42d120b7ebd270c98db31ee5ca0
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:27:37 +0500

    Add files via upload

---
+++ Exercise 7/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const additionResult = 4 + 4;
+console.log("Addition Result: ", additionResult);
+const subtractionResult = 16 - 8;
+console.log("Subtraction Results:" + subtractionResult);
+const multiplicationResult = 4 * 2;
+console.log("Multiplications result:" + multiplicationResult);
+const divisionResult = 64 / 8;
+console.log("Division Result:" + divisionResult);
---
+++ Exercise 7/index.ts
@@ -0,0 +1,23 @@
+/*Number Eight: Write addition, subtraction, multiplication,
+ and division operations that each result in the number 8. Be sure to enclose your operations
+  in print statements to see the results.*/
+
+
+
+
+
+// addition operations that each result in the number 8. 
+const additionResult = 4 + 4;
+console.log("Addition Result: ",additionResult)
+
+// subtraction operations that each result in the number 8. 
+const subtractionResult = 16-8
+console.log("Subtraction Results:" + subtractionResult)
+
+// multiplication operations that each result in the number 8. 
+const multiplicationResult = 4*2;
+console.log("Multiplications result:" + multiplicationResult)
+
+// division operations that each result in the number 8. 
+const divisionResult = 64 / 8;
+console.log("Division Result:" + divisionResult) 
