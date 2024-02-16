159f5dcbf3ce82cc55045eca039076cc0bed3387
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:32:00 +0500

    Add files via upload

---
+++ Exercise 19/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// importing Guest List from exercise 15
+const Exercise_15_1 = require("../Exercise 15");
+console.log(`\nprinting message indicating number of guests`);
+console.log(`We had finally invited ${Exercise_15_1.guestList.length} guest in Exercise 15.`);
---
+++ Exercise 19/index.ts
@@ -0,0 +1,9 @@
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message
+ indicating the number of people you are inviting to dinner.*/ 
+
+
+// importing Guest List from exercise 15
+import{ guestList } from '../Exercise 15'
+
+console.log(`\nprinting message indicating number of guests`)
+console.log(`We had finally invited ${guestList.length} guest in Exercise 15.`)
