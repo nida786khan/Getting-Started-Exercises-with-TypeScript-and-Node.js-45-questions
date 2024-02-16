8c65937a4189cf72aedf0bef66b1ab2328eccd38
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:29:17 +0500

    Add files via upload

---
+++ Exercise 12/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = [`Nida`, `Ajab`, `Ahmed`, `Khan`];
+for (let name of names) {
+    console.log(`Good morning ${names}, I hopeyou are having a fantastic day!`);
+}
---
+++ Exercise 12/index.ts
@@ -0,0 +1,11 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+print a message to them. The text of each message should be the same, but each message should be personalized
+ with the person’s name.*/
+
+
+
+let names: string[]=[`Nida`,`Ajab`,`Ahmed`,`Khan`];
+for (let name of names)
+{
+  console.log(`Good morning ${names}, I hopeyou are having a fantastic day!`)
+}
