cb3e695794a773b6b13667acbdea0dfa29895de2
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:39:15 +0500

    Add files via upload

---
+++ Exercise 41/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Array of magician's names
+const magicianNames = ["Harry Porter", "David Copperfield", "Hermione Granger", "Professor Albus Dumbledore", "Ron Weasley"];
+function show_magicians(magicians) {
+    for (const items of magicians) {
+        console.log(items);
+    }
+}
+// call the function to show the magician's names
+show_magicians(magicianNames);
