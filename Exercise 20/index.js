79c695fe506ccf70c4f35000cf027c1b0743e29d
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:32:19 +0500

    Add files via upload

---
+++ Exercise 20/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const countries = ["USA", "Pakistan", "Australia", "Japan", "Chaina"];
+console.log("List of Countries:");
+for (let country of countries) {
+    console.log(country);
+}
---
+++ Exercise 20/index.ts
@@ -0,0 +1,10 @@
+/*Think of something you could store in a array. For example, you could make a list of
+ mountains, rivers, countries, cities, languages, or anything else you’d like.
+ Write a program that creates a list containing these items.*/
+
+const countries: string[] = ["USA","Pakistan","Australia","Japan","Chaina"]
+console.log("List of Countries:")
+for (let country of countries)
+{
+  console.log(country)
+}
