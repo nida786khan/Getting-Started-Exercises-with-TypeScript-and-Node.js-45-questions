16c69f65a937c4cd3f3bd3da606a7fe121bfcc95
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:23:29 +0500

    Add files via upload

---
+++ Exercise 2/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Nida";
+let message = "Get ready ${personName}, You have to solve 45 exercises of TypeScript.";
+console.log(message);
---
+++ Exercise 2/index.ts
@@ -0,0 +1,8 @@
+/*Personal Message: Store a person’s name in a variable, and print a message to that person. 
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+
+
+
+let personName: string = "Nida"
+let message: string = "Get ready ${personName}, You have to solve 45 exercises of TypeScript."
+console.log(message)
