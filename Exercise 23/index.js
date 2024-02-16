b90437dde1ccc27ee0a486a182a5b9eacfe6974e
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:33:30 +0500

    Add files via upload

---
+++ Exercise 23/index.js
@@ -0,0 +1,23 @@
+"use strict";
+let car = 'subaru';
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'Sbaru'); // True
+console.log("Is car == 'honda' ? I predict False.");
+console.log(car == 'honda'); // False
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'sbaru'); // True
+console.log("Is car == 'subaru' ? I predict False.");
+console.log(car == 'Sbaru'); // False
+console.log("Is car !== 'toyota' ? I predict True.");
+console.log(car !== 'toyota'); // True
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== 'Sbaru'); // False
+console.log("Is car !== 'mazda' ? I predict True.");
+console.log(car !== 'mazda'); // True
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== 'Sbaru'); // False
+let age = 25;
+console.log("Is age > 18? I predict Ture.");
+console.log(age > 18); // Ture
+console.log("Is age <21? I predict False.");
+console.log(age < 21); // False
---
+++ Exercise 23/index.ts
@@ -0,0 +1,48 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement
+ describing each test and your prediction for the results of each test.
+  Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'Sbaru'); // True
+
+console.log("Is car == 'honda' ? I predict False.");
+console.log(car == 'honda'); // False
+
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'sbaru'); // True
+
+console.log("Is car == 'subaru' ? I predict False.");
+console.log(car == 'Sbaru'); // False
+
+console.log("Is car !== 'toyota' ? I predict True.");
+console.log(car !== 'toyota'); // True
+
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== 'Sbaru'); // False
+
+console.log("Is car !== 'mazda' ? I predict True.");
+console.log(car !== 'mazda'); // True
+
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== 'Sbaru'); // False
+
+let age = 25;
+console.log("Is age > 18? I predict Ture.");
+console.log(age > 18); // Ture
+
+console.log("Is age <21? I predict False.");
+console.log(age < 21); // False
+
