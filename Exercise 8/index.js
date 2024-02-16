ec9eef4e7fbc9808e10068005055ff146ea7c00a
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:27:59 +0500

    Add files via upload

---
+++ Exercise 8/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("-".repeat(80));
+console.log(5 + 3);
+console.log("_".repeat(80));
+console.log(16 - 8);
+console.log("=".repeat(80));
+console.log(4 * 2);
+console.log("-".repeat(80));
+console.log(16 / 2);
---
+++ Exercise 8/index.ts
@@ -0,0 +1,15 @@
+/*You should create four lines that look like this:
+
+console.log(5 + 3)
+
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+
+
+console.log("-".repeat(80))
+console.log(5 + 3)
+console.log("_".repeat(80))
+console.log(16 - 8)
+console.log("=".repeat(80))
+console.log(4 * 2)
+console.log("-".repeat(80))
+console.log(16 / 2)
