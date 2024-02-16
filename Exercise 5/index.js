db3ec99b7078c2326c1de5de4987e8fd2fa57e28
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:25:29 +0500

    Add files via upload

---
+++ Exercise 5/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Quid e Azam";
+let message = famous_person + ' once said, “Expect the best, prepare for the worst.”';
+console.log(message);
---
+++ Exercise 5/index.ts
@@ -0,0 +1,8 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable
+ called famous_person. Then compose your message and store it in a new
+ variable called message. Print your message.*/
+
+
+let famous_person: string = "Quid e Azam";
+let message: string = famous_person +' once said, “Expect the best, prepare for the worst.”'
+console.log(message);
