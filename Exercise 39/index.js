eb630574dee54b1f74ccac9a836874d994dd5f62
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:38:39 +0500

    Add files via upload

---
+++ Exercise 39/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const city_country = (city, country) => {
+    return `${city}, ${country}`;
+};
+// Test the function with with three city-country pairs
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("New York", "USA"));
+console.log(city_country("Tokyo", "Japan"));
---
+++ Exercise 39/index.ts
@@ -0,0 +1,16 @@
+/*City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+"Lahore, Pakistan"
+
+Call your function with at least three city-country pairs, and print the value that’s returned.*/
+
+const city_country = (city: string, country: string)
+: string =>
+{
+  return `${city}, ${country}`;
+}
+
+// Test the function with with three city-country pairs
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("New York", "USA"));
+console.log(city_country("Tokyo", "Japan"));
