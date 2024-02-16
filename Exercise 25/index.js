d0c515542b452a789bc9d55c57e6dbcd835f8af3
Nida Khan <147870855+nida786khan@users.noreply.github.com>
2024-02-17 03:34:09 +0500

    Add files via upload

---
+++ Exercise 25/index.js
@@ -0,0 +1,15 @@
+"use strict";
+let alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("Congratulations! You juest earned 5 point.");
+}
+// second version
+alien_color = 'blue';
+if (alien_color == 'green') {
+    console.log("Congratulation! You just earned 5 points.");
+}
+// second version
+alien_color = 'blue';
+if (alien_color == 'green') {
+    console.log("Congratulation! You just earned 5 points.");
+}
